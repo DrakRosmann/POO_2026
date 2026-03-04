094649541b8c544d6f34bf66ca53686f7be9e697
Guilherme de Lima Rosmann <45110117+DrakRosmann@users.noreply.github.com>
2026-03-04 14:01:48 +0000

    First commit

---
+++ helloworld/src/main/java/com/guir/App.java
@@ -0,0 +1,9 @@
+package com.guir;
+
+public class App {
+    public static void main(String[] args) {
+        Pessoa p = new Pessoa("Guilherme", 21);
+        System.out.println("Hello, world! Projeto helloworld criado com sucesso.");
+        System.out.println(p);
+    }
+}
