4234005bdc5f0071142732dc951c510ceb30afd8
Guilherme de Lima Rosmann <45110117+DrakRosmann@users.noreply.github.com>
2026-03-14 13:07:32 +0000

    Conclu

--- aula11_03/src/main/java/com/guir/App.java
+++ aula11_03/src/main/java/com/guir/App.java
@@ -5,37 +5,64 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.printf("Cadastrar usuario.\nNome: ");
+        System.out.println("Cadastrar cliente.");
+        System.out.print("Nome: ");
         String nome = in.next();
-        System.out.printf("CPF: ");
+        System.out.print("CPF: ");
         String cpf = in.next();
+        Cliente cliente = new Cliente(nome, cpf);
+        Boolean loop;
 
-        Cliente cliente = new Cliente(cpf, nome);
- 
-    
-
-    }
-    public static void pontuacaoCompra(Cliente cliente, double valorCompra){
-        if(valorCompra <= 100){
-            pontuacao += 1;
-        }
-        else if(valorCompra > 101 && valorCompra <= 500){
-            pontuacao += (valorCompra/100);
-        }
-        else if(valorCompra > 500){
-            pontuacao += (valorCompra/100);
-        }
+        do { 
+            loop = menu(in, cliente);
+        } while (loop);
     }
 
-    public static double desconto(){
-        if(pontuacao >= 10 && pontuacao <= 50){
-            return valorCompra - (valorCompra*0.10);
+    public static Boolean menu(Scanner in, Cliente cliente){
+        System.out.println("Selecione uma das opções: ");
+        System.out.println("1 - Comprar");
+        System.out.println("2 - Consultar pontuação.");
+        System.out.println("3 - Sair.");
+        System.out.print("SEL: ");
+        int sel = in.nextInt();
+        switch (sel) {
+            case 1:
+                comprar(in,cliente);
+                break;
+            case 2:
+                System.out.printf("Você possui %d pontos.\n", cliente.getPontuacao());
+                break;
+            case 3:
+                System.out.println("Saindo...");
+                return false;
+            default:
+                System.out.println("Você digitou errado.");
+                break;
         }
-        else if (pontuacao >50){
-            return valorCompra - (valorCompra*0.20);
-        }
-        else{
-            return valorCompra;
+        System.out.print("Realizar outra operação (1-SIM | 2-NÃO): ");
+        if (in.nextInt() == 1) return true;
+        System.out.println("Saindo...");
+        return false;
+    }
+
+    public static void comprar(Scanner in,Cliente cliente){
+        System.out.print("Insira o valor do produto: R$ ");
+        cliente.setValorCompra(in.nextDouble());
+        if (cliente.getPontuacao() < 10) {
+            System.out.printf("Você ganhou %d pontos.\n", cliente.calcPontos());
+            cliente.acumularPontos();
+        }else{
+            System.out.printf("Você possui direito a %d pontos ou R$ %.1f de desconto.\n",cliente.calcPontos(),cliente.calcDesconto());
+            System.out.println("1 - Acumular pontos.");
+            System.out.println("2 - Obter desconto.");
+            System.out.print("SEL: ");
+            if(in.nextInt() == 1){
+                System.out.printf("Você ganhou %d pontos.\n",cliente.calcPontos());
+                cliente.acumularPontos();
+            }else{
+                cliente.obterDesconto();
+                System.out.printf("Valor total com desconto: R$ %.1f",cliente.getValorCompra());
+            }
         }
     }
 }
--- aula11_03/src/main/java/com/guir/Cliente.java
+++ aula11_03/src/main/java/com/guir/Cliente.java
@@ -1,22 +1,15 @@
 package com.guir;
-
 public class Cliente {
-    private String cpf;
     private String nome;
+    private String cpf;
+    private double valorCompra;
     private int pontuacao;
-    
-    public Cliente(String cpf, String nome) {
-        this.cpf = cpf;
+
+    public Cliente(String nome, String cpf) {
         this.nome = nome;
-        pontuacao = 0;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
         this.cpf = cpf;
+        valorCompra = 0;
+        pontuacao = 0;
     }
 
     public String getNome() {
@@ -27,10 +20,44 @@ public class Cliente {
         this.nome = nome;
     }
 
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
     public int getPontuacao() {
         return pontuacao;
     }
 
+    public void setValorCompra(double valorCompra) {
+        this.valorCompra = valorCompra;
+    }
+    
+    public double getValorCompra() {
+        return valorCompra;
+    }
 
+    public int calcPontos(){
+        if(valorCompra <= 100) return 1;
+        else if(valorCompra >= 101 && valorCompra <= 500) return (int) valorCompra/100;
+        else return (int) valorCompra/100;
+    }
 
+    public double calcDesconto(){
+        if(pontuacao >= 10 && pontuacao <= 50) return valorCompra*0.10;
+        else if (pontuacao >50) return valorCompra*0.2;
+        else return 0;
+    }
+
+    public void acumularPontos(){
+        pontuacao += calcPontos();
+    }
+
+    public void obterDesconto(){
+        valorCompra -= calcDesconto();
+        pontuacao -= 10;
+    }
 }
