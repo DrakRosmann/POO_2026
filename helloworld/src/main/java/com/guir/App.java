2393a27336a9a485283c3a08a4e79ede62cc42ba
Guilherme de Lima Rosmann <45110117+DrakRosmann@users.noreply.github.com>
2026-03-14 13:09:09 +0000

    Clean

--- helloworld/src/main/java/com/guir/App.java
+++
@@ -1,9 +0,0 @@
-package com.guir;
-
-public class App {
-    public static void main(String[] args) {
-        Pessoa p = new Pessoa("Guilherme", 21);
-        System.out.println("Hello, world! Projeto helloworld criado com sucesso.");
-        System.out.println(p);
-    }
-}
