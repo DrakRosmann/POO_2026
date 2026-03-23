5ebdd6328641f25ad40bcec0bc7e7d30c2065e97
Guilherme de Lima Rosmann <45110117+DrakRosmann@users.noreply.github.com>
2026-03-23 19:44:38 -0300

    Agencia Bancaria

---
+++ contacorrente/src/main/java/com/guir/AgenciaBancaria.java
@@ -0,0 +1,60 @@
+package com.guir;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AgenciaBancaria {
+    private int numero;
+    private String nome;
+    private List<ContaCExtrato> contasBan;
+
+    public AgenciaBancaria(int numero, String nome) {
+        this.numero = numero;
+        this.nome = nome;
+        contasBan = new ArrayList<>();
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<ContaCExtrato> getContasBan() {
+        return contasBan;
+    }
+
+    public void cadastrarConta(ContaCExtrato conta){
+        if(contasBan.contains(conta)){
+            throw new IllegalArgumentException("Conta já cadastrada.");
+        }
+        contasBan.add(conta);
+    }
+
+    public ContaCExtrato recuperarConta(int numero){
+        for (ContaCExtrato contas : contasBan){
+            if (contas.getNumeroConta() == numero) return contas;
+        }
+        return null;
+    }
+
+    public void encerrarConta(int numero){
+        try {
+            for (ContaCExtrato contas : contasBan) {
+                if (contas.getNumeroConta() == numero) contas.encerramento();
+            }
+        }catch (IllegalArgumentException e){
+            throw new IllegalArgumentException("Não é possível o encerramento.");
+        }
+    }
+}
