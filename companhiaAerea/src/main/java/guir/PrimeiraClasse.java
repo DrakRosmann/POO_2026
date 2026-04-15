9322bdff15bf4dcf687f1221bf71ccfb302d7842
Guilherme de Lima Rosmann <45110117+DrakRosmann@users.noreply.github.com>
2026-04-15 20:35:59 -0300

    update

--- companhiaAerea/src/main/java/guir/PrimeiraClasse.java
+++ companhiaAerea/src/main/java/guir/PrimeiraClasse.java
@@ -1,4 +1,10 @@
 package guir;
 
+import java.time.LocalDate;
+
 public class PrimeiraClasse extends Passagem{
+
+    public PrimeiraClasse(double custoBase, LocalDate data, int numeroVoo) {
+        super((custoBase*0.1)+custoBase, data, numeroVoo, 0, false);
+    }
 }
